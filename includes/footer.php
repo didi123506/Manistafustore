712fcbac14e76fe303bb456ac740fd5dd377f069
didi123506 <deemanisone@gmail.com>
2024-10-14 22:57:54 +0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+<body>
+    <script src="assets/js/jquery-3.7.1.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/custom.js"></script>
+    
+    <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/alertify.min.js"></script>
+    <script>
+          alertify.set('notifier','position', 'top-right');
+        <?php 
+        if(isset($_SESSION['message'])) 
+        { 
+          ?>
+        alertify.success('<?= $_SESSION['message']; ?>');
+        <?php 
+        unset($_SESSION['message']);
+        } 
+        ?>
+      </script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/custom.css" rel="stylesheet">
+
+    <!-- Page Title -->
+    <title>Hello, welcome</title>
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Sofadi+One&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+  
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/alertify.min.css"/>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/themes/bootstrap.min.css"/>
+    
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+    <style>
+      /* Apply new font styles */
+      body {
+        font-family: 'Lamoon',  sans-serif; /* Default body font */
+      }
+      h1, h2, h3, h4, h5, h6 {
+        font-family: 'Lamoon', sans-serif; /* Headers font */
+      }
+      .navbar-brand {
+        font-family: 'phetsarath OT', sans-serif; /* Brand name or logo font */
+      }
+
+      /* Ensure anchor tags have no underline */
+      a {
+        text-decoration: none;
+      }
+    </style>
+  </head>
+  <body>
+    <?php 
+      include('navbar.php');
+    ?>
