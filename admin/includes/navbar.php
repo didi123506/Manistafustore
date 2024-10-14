96ce6f5878bce13dcccad0965b892205ee1ffe78
didi123506 <deemanisone@gmail.com>
2024-10-14 23:08:09 +0700

    Add files via upload

---
+++ admin/includes/navbar.php
@@ -0,0 +1,11 @@
+
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
+      <div class="container-fluid py-1 px-3">
+        
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+          
+          </div>
+          
+        </div>
+      </div>
+    </nav>
